616c062937b6644dfe1e9466f78890d72f91c8e0
Fabian Zamudio <fabian.zamudio.89@gmail.com>
2023-11-25 20:57:42 -0300

    First commit

---
+++ src/main/java/com/fzamudio/employeeplatform/EmployeePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.fzamudio.employeeplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePlatformApplication.class, args);
+	}
+
+}
